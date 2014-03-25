98d5ef2330edba7b87bebd45387cda83b97d36c3
Amanpreet Singh <amanpreet.singh@flipkart.com>
2014-03-25 23:39:22 +0530

    Initial Ruby Module Commit.

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ TimeTableGenerator
 ==================
 
 Time Table Generator is an application which generates time table for schools and colleges within a fraction of seconds.
+
+
+##Some git related instructions.
+*   Keep your checkin messages meaningful.
+*   Try to work on branches when developing features and avoid checking directly into master.
+    *   Later Merge them using reverse merges and squashes.
+    *   Try to get changes reviewed by one of the peer before merging into master
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TimeTableGenerator::Application.config.secret_key_base = 'cea7ec36c1950eb95ebc8347f66282fda69a056c658f14fc186614d0c990e4ab8d0768582d27232c1a960f8a22fb099e74d53036e6e3c4f200d36c7a52bfce47'
