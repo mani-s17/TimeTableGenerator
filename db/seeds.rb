67d94e1dbcecce2f39f523ac8abcd4ef6d63d0a0
Amanpreet Singh <coool.aman@gmail.com>
2014-04-05 16:49:56 +0530

    Created Seed Task to pre populate Sample Data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+namespace :db do
+  task seed: :environment do
+
+    #Base entities
+    5.times { FactoryGirl.create(:teacher) }
+    5.times { FactoryGirl.create(:subject) }
+    3.times { FactoryGirl.create(:standard) }
+
+    #Linkings
+    Standard.all.each do |standard|
+      4.times { |n| FactoryGirl.create(:class_group, name: "CG#{n}", standard: standard) }
+    end
+
+    ClassGroup.all.each do |group|
+      Subject.all.each do |subject|
+        FactoryGirl.create(:group_subject, class_group: group, subject: subject)
+      end
+    end
+
+    Standard.all.each do |standard|
+      Subject.all.each do |subject|
+        FactoryGirl.create(:subject_standard, standard: standard, subject: subject)
+      end
+    end
+
+    Teacher.all.each do |teacher|
+      SubjectStandard.all.each do |subject_standard|
+        FactoryGirl.create(:teacher_subject_standard, teacher: teacher, subject_standard: subject_standard)
+      end
+    end
+
+  end
+end
