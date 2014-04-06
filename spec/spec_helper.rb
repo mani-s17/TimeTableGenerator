3c576fe8332d0c0048759f344cc3baa83480298e
Amanpreet Singh <coool.aman@gmail.com>
2014-04-06 18:09:55 +0530

    Added application helper spec and supporting utility file.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,6 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -39,4 +38,10 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+  # Include the Capybara DSL so that specs in spec/requests still work.
+  config.include Capybara::DSL
+  # Disable the old-style object.should syntax.
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
 end
