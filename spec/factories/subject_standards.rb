dde16746325cac7858396785015a1eb5b6b95c16
Amanpreet Singh <coool.aman@gmail.com>
2014-04-06 23:04:44 +0530

    Improved factories to incorporate has_many relationships.

--- spec/factories/subject_standards.rb
+++ spec/factories/subject_standards.rb
@@ -4,5 +4,17 @@ FactoryGirl.define do
   factory :subject_standard do
     subject
     standard
+
+    factory :subject_standard_with_code do
+      ignore do
+        code nil
+      end
+
+      before(:create) do |subject_standard, evaluator|
+        subject_standard.subject.name=evaluator.code
+        subject_standard.subject.code=evaluator.code
+        subject_standard.subject.save
+      end
+    end
   end
 end
--- spec/factories/teachers.rb
+++ spec/factories/teachers.rb
@@ -4,5 +4,28 @@ FactoryGirl.define do
   factory :teacher do
     sequence(:name) { |n| "Teacher #{n}" }
     teaching_hours Random.rand(10)
+
+    # teacher_subject_standards will create data after the teacher has been created
+    factory :teacher_with_subjects do
+
+      # Ignored attribute and available to be used in creation
+      ignore do
+        standards 2
+        subjects ['PHY', 'CHEM', 'MAT']
+      end
+
+      # the after(:create) yields two values; the teacher instance itself and the
+      # evaluator, which stores all values from the factory, including ignored
+      # attributes;
+      after(:create) do |teacher, evaluator|
+        evaluator.standards.times do
+          standard = create(:standard)
+          evaluator.subjects.each do |subject_code|
+            subject_standard = create(:subject_standard_with_code, standard: standard, code: subject_code)
+            create(:teacher_subject_standard, teacher: teacher, subject_standard: subject_standard)
+          end
+        end
+      end
+    end
   end
 end
