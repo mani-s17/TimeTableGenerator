df61f3a721b055e11bae31485455e24df6d2d458
Amanpreet Singh <coool.aman@gmail.com>
2014-04-07 00:29:28 +0530

    Temporary display for input jsons added to getTimeTable API.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,5 +3,7 @@ include ApiHelper
 class ApiController < ApplicationController
   def getTimeTable
     @teachers_json = ActiveSupport::JSON.encode(extract_teacher_map_all)
+    @class_groups_json = ActiveSupport::JSON.encode(extract_class_group_map_all)
   end
+
 end
