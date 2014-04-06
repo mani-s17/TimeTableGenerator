3c983b8e84b9053b3039d6e92f0c23dfb97d5e73
Amanpreet Singh <coool.aman@gmail.com>
2014-04-07 00:14:04 +0530

    Enhanced Factories to include has_many relationships.

--- spec/factories/class_groups.rb
+++ spec/factories/class_groups.rb
@@ -5,5 +5,20 @@ FactoryGirl.define do
     sequence(:name) { |n| "Group #{n}" }
     standard
     size Random.rand(40)
+
+    factory :class_group_with_subjects do
+
+      ignore do
+        hours 4
+        subjects ['PHY', 'CHEM', 'MAT']
+      end
+
+      after(:create) do |class_group, evaluator|
+        evaluator.subjects.each do |subject_code|
+          create(:group_subject_with_code, class_group: class_group, hours_required: evaluator.hours, code: subject_code)
+        end
+      end
+    end
+
   end
 end
--- spec/factories/group_subjects.rb
+++ spec/factories/group_subjects.rb
@@ -5,5 +5,17 @@ FactoryGirl.define do
     class_group
     subject
     hours_required Random.rand(1..10)
+
+    factory :group_subject_with_code do
+      ignore do
+        code nil
+      end
+
+      before(:create) do |group_subject, evaluator|
+        group_subject.subject.name=evaluator.code
+        group_subject.subject.code=evaluator.code
+        group_subject.subject.save
+      end
+    end
   end
 end
