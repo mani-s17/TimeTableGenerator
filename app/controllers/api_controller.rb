90702c1d4c4dbb4e07f611478d7e841c105aec37
Amanpreet Singh <coool.aman@gmail.com>
2014-04-10 19:18:40 +0530

    Showed combined json on getTimeTable View.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,8 +2,7 @@ include ApiHelper
 
 class ApiController < ApplicationController
   def getTimeTable
-    @teachers_json = ActiveSupport::JSON.encode(extract_teacher_map_all)
-    @class_groups_json = ActiveSupport::JSON.encode(extract_class_group_map_all)
+    @combined_json = ActiveSupport::JSON.encode(extract_api_input)
   end
 
 end
