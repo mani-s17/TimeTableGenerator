d369c1e377a5e2bdb35611e29de402d50357c67f
Amanpreet Singh <coool.aman@gmail.com>
2014-04-12 19:03:31 +0530

    Integrated with Service EndPoint and displayed output on View.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,8 +1,21 @@
 include ApiHelper
+require 'net/http'
 
 class ApiController < ApplicationController
   def getTimeTable
-    @combined_json = ActiveSupport::JSON.encode(extract_api_input)
+    @combined_json = extract_api_input.to_json
+    #TODO:Move Url to Config
+    url = 'http://timetable-generator.mani-s17.cloudbees.net/rest/generate/'
+    response = do_post(url,@combined_json)
+    @api_output = "Response #{response.code} #{response.message}: #{response.body}"
   end
 
+  private def do_post(url,json_data)
+    uri = URI(url)
+    req = Net::HTTP::Post.new(uri)
+    # req.basic_auth @user, @pass
+    req.content_type = 'application/json'
+    req.body = json_data
+    Net::HTTP.new(uri.hostname, uri.port).request(req)
+  end
 end
